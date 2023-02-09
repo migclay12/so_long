fe89b7e35f365595f82b4a1b28310b43f2bfc0b3
Miguel Gonzalez Clayton <miggonza@c1r17s6.42madrid.com>
2023-02-09 16:58:28 +0100

    map

--- map/map.h
+++ map/map.h
@@ -4,12 +4,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "../moving_figure/define.h"
 
 typedef struct s_map
 {
+    char **matrix;
     int x;
     int y;
 } t_map;
+
+typedef struct s_things {
+	t_map		map;
+}	t_things;
 
 int	ft_putstr(char	*s);
 int	ft_putchar(int c);
--- map/map_read.c
+++ map/map_read.c
@@ -1,22 +1,9 @@
-//WTF AM I DOING?!
-//Guardar x e y en estructura
+//ME GUARDA UN CARACTER DE MAS EN LA X
 #include <fcntl.h>
 #include "map.h"
 #include "long_gnl/get_next_line.h"
 //#include "../libft/libft.h"
-/*
-void	*ft_memset(void *b, int c, size_t len)
-{
-	unsigned char	*temp;
-	size_t			i;
 
-	i = 0;
-	temp = (unsigned char *)b;
-	while (i < len)
-		temp[i++] = (unsigned char)c;
-	return (b);
-}
-*/
 
 void	ft_print_error(char *str)
 {
@@ -45,44 +32,106 @@ void ft_map_size(int fd, t_map *size)
 	//printf("%d\n", size->y);
 }
 
-char **ft_read_map (int fd, int height)
+
+char **ft_read_map(int fd, t_map *map)
 {
-	char *map;
-	char **full_map;
+	//char **full_map;
 	int lines;
 
-	//map = get_next_line(fd);
+	map -> matrix = (char **)malloc(sizeof(char *) * map -> x + 1);
 	lines = 0;
-	while (map != NULL)
+	while (lines < map -> x)
 	{
-		full_map[lines] = map;
-		map = get_next_line(fd);
+		map -> matrix[lines] = get_next_line(fd);
 		lines++;
+	}
+	return (map -> matrix);
+}
+
+int ft_map_name(char *map)
+{
+	int len;
+
+	len = ft_strlen(map);
+	if (map[len - 1] == 'r' && map[len - 2] == 'e'
+		&& map[len - 3] == 'b' && map[len - 4] == '.')
+		return (1);
+	return (0);
+}
+
+//se puede hacer void?
+int ft_wall_check(t_map *map)
+{
+	int i;
+	int j;
+	int top;
+	int bot;
+
+	i = 0;
+	while (map->matrix[0][i] != '\n')
+	{
+		top = 1;
+		if (map->matrix[0][i] != WALL)
+			return (0);
+		i++;
 	}
 
-	lines = 0;
-	while (lines < height)
+	j = 0;
+	while (map->matrix[map->y - 1][j] != '\n')
 	{
-		printf("%c", full_map[lines][2]);
-		lines++;
+		bot = 1;
+		if (map->matrix[map->y - 1][j] != WALL)
+			return (0);
+		j++;
 	}
-	return (full_map);
+	if (top == 1 && bot == 1)
+		return (1);
+	return (0);
+}
+
+int ft_mid_wall_check(t_map *map)
+{
+	int i;
+
+	i = 1;
+	while (i < map->y - 1)
+	{
+		if (map->matrix[i][0] != WALL || map->matrix[i][map->x - 2] != WALL)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	int fd;
+	t_things things;
 	t_map maps;
+	int fd;
 	char **idk;
-	int fdy;
+	int name;
+	int wall;
 	//ft_memset(&maps, 0, sizeof(t_map));
 	
 	fd = open("map0.ber", O_RDONLY);
 	ft_map_size(fd, &maps);
 	close (fd);
 
-	fdy = open("map0.ber", O_RDONLY);
-	idk = ft_read_map(fdy, maps.y);
-	close (fdy);
+	fd = open("map0.ber", O_RDONLY);
+	idk = ft_read_map(fd, &maps);
+	//printf("%c", idk[1][11]);
+	close (fd);
+
+	name = ft_map_name("map0.ber");
+	if (name != 1)
+		ft_print_error("map is not .ber file");
+	
+	//wall = ft_wall_check(&maps);
+	//if (wall != 1)
+		//ft_print_error("map is not surrounded by walls");
+
+	wall = ft_mid_wall_check(&maps);
+	if (wall != 1)
+		ft_print_error("map is not surrounded by walls");
 	return (0);
 }
